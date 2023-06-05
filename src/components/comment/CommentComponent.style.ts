043d862ef19fa58314ed30b324ff9e2bee01c47c
rkumarr1923 <rkumarr1923@gmail.com>
2023-06-05 14:53:26 +0530

    comment feature UI update

--- src/components/comment/CommentComponent.style.ts
+++ src/components/comment/CommentComponent.style.ts
@@ -10,3 +10,51 @@ export const ReplyWrapper = styled.p`
 export const ReplyLeftPadding = styled.p`
     margin-left: ${count= count+1}em;
 `;
+
+export const ListWrapper = styled.p`
+    background-color: white;
+    border-radius: 15px;
+    text-align: left;
+    padding: 1em;
+`;
+
+export const TimeWrapper = styled.span`
+    color: #fff;
+    background-color: #1ab394;
+    border-color: #1ab394;
+    border-radius: 15px;
+    width: 30%;
+    height: 16px;
+    text-align: center;
+    float: inherit;
+    font-size: 10px;
+    box-shadow: 0px 3px #929598;
+`;
+
+
+export const UserWrapper = styled.span`
+    color: #fff;
+    background-color: #2677c7;
+    border-color: #ffffff;
+    border-radius: 6px;
+    width: 68%;
+    height: 20px;
+    text-align: center;
+    float: inherit;
+    font-size: 12px;
+    /* box-shadow: 0px 3px #929598; */
+    padding: 7px;
+`;
+
+export const NBSP = styled.span`
+    color: #fff;
+    background-color: #1ab394;
+    border-color: #1ab394;
+    border-radius: 15px;
+    width: 30%;
+    height: 16px;
+    text-align: center;
+    float: inherit;
+    font-size: 10px;
+    box-shadow: 0px 3px #929598;
+`;
